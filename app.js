ebc4cb0b98fb5d07275a6357a4fcd6928db89d1d
brnguy <nguyen.bryan15@gmail.com>
2023-10-26 16:59:15 -0700

    Added GET routes to app.js

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("IT WORKS");
+app.get('/products', (req, res, next) => {
+    res.send("<h1>Products Page</h1>");
+})
+
+app.get('/cart', (req, res, next) => {
+    res.send("<h1>My Cart</h1>");
+})
+
+app.get('/', (req, res, next) => {
+    res.send("<h1>Index</h1>");
 })
 
 app.listen(3000);
