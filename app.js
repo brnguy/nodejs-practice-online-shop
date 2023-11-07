9ee4f207528fce6c6ef70e58131ce42d288b0ff9
brnguy <nguyen.bryan15@gmail.com>
2023-11-07 11:57:35 -0800

    Refactored certain routes to shopRoutes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const shopRoutes = require('./routes/shop');
+
 app.get('/products', (req, res, next) => {
     res.send("<h1>Products Page</h1>");
 })
@@ -10,8 +12,6 @@ app.get('/cart', (req, res, next) => {
     res.send("<h1>My Cart</h1>");
 })
 
-app.get('/', (req, res, next) => {
-    res.send("<h1>Index</h1>");
-})
+app.use(shopRoutes);
 
 app.listen(3000);
